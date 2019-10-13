6132501fedf891ecc5fe931913021f22fef0807d
Josselyn2008 <56282532+Josselyn2008@users.noreply.github.com>
2019-10-13 12:13:14 -0500

    Add files via upload

---
+++ ColumnasNumero/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ColumnasNumero
+{
+    class Program
+    {
+        //   7. Crear un programa que escriba dos columnas de números, en la primera se colocan los números del 1 al
+        //   100, en la segunda los números del 100 al 1 
+
+        static void Main(string[] args)
+        {  
+            //Variables
+
+            int i, j = 101;
+
+            for (i = 1; i <= 100; i++)
+            {
+                j = j - 1;
+                Console.WriteLine(i + "    " + j);
+            }
+        }
+    }
+}
