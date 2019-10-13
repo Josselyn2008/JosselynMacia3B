11c946f7d7ad9532264276ce550811200c0392e5
Josselyn2008 <56282532+Josselyn2008@users.noreply.github.com>
2019-10-13 12:01:16 -0500

    Add files via upload

---
+++ Piramide/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Piramide
+{
+    //5. Crear el programa asteriscos4 en el que se introduce un número entero y se crea una pirámide de
+    // asteriscos.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            int alt, x, y;
+            Console.WriteLine(" Ingrese la altura con la cual quiere realizar su piramide ");
+            alt = int.Parse(Console.ReadLine());
+
+
+            for(x = 0; x <= alt; x++)
+            {
+                for(y=0; y < alt - x; y++)
+                {
+                    Console.Write(" ");
+                }
+                for (y = 0; y < 2 * x - 1; y++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
