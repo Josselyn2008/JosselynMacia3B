83424b5a8f8211e7657b7f80f98b4382e26e77ea
Josselyn2008 <56282532+Josselyn2008@users.noreply.github.com>
2019-10-13 12:08:48 -0500

    Add files via upload

---
+++ Rombo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Rombo
+{
+    class Program
+    {
+       // 6. Crear el programa asteriscos en el que se introduce un número entero y se crea un rombo de
+       // asteriscos
+
+        static void Main(string[] args)
+        {
+            int x, y, tam; //Variables
+            Console.WriteLine("Ingrese el tamaño del que quiere su Rombo");
+            tam = int.Parse(Console.ReadLine());
+
+            //Parte superior
+
+            for(x=1; x<=tam; x++)
+            {
+                for(y=1; y <= tam- x; y++)
+                {
+                    Console.Write(" ");
+                }
+                for(y=1; y <= 2 * x - 1; y++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            //Parte inferior
+
+            for (x = tam-2; x >= 0; x--)
+            {
+                for (y = 1; y <= tam- x-1; y++)
+                {
+                    Console.Write(" ");
+                }
+                for (y = 0; y < 2 * x + 1; y++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
