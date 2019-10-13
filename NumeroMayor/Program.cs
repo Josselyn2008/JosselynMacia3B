4edd1bbec26b1f3e88645cebac9fe6f7a4c6c167
Josselyn2008 <56282532+Josselyn2008@users.noreply.github.com>
2019-10-13 11:06:25 -0500

    Add files via upload

---
+++ NumeroMayor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumeroMayor
+{
+  
+    class Program {
+        static void Main(string[] args){
+ // 1. Programa que lea una serie de números por teclado e indique cuál es el mayor
+            int C = 0;
+            Console.WriteLine("Escriba la cantidad total de numeros a ingresar ");
+            int A = int.Parse(Console.ReadLine());
+            for (int i = 0; i < A; i++) {
+                Console.WriteLine("Ingrese numero");
+                int B = int.Parse(Console.ReadLine());
+                if (i == 0){
+                    C = B;
+                }
+                else {
+                    if (i != 0){
+                        if (B > C){
+                            C = B;
+                        }
+                    }
+                } 
+            }
+            Console.WriteLine("El numero mayor es " + C);
+            Console.ReadLine();
+        }
+    }
+}
