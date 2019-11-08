2805a66e7e8922991b12fd50bf80edda136d3d45
Josselyn2008 <56282532+Josselyn2008@users.noreply.github.com>
2019-11-07 21:30:29 -0500

    Add files via upload

---
+++ Programa_Integrado/Tads/Cola.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programa_Integrado.Tads
+{
+    class Cola
+    {
+        private int[] dato = new int[5];
+        public int[] Dato
+        {
+            get { return dato; }
+            set { dato = value; }
+        }
+        private int ultimo = -1;
+
+        public int Ultimo
+        {
+            get { return ultimo; }
+            set { ultimo = value; }
+        }
+        private int primero = -1;
+
+        public int Primero
+        {
+            get { return primero; }
+            set { primero = value; }
+        }
+
+        internal void cola()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EliminarDato()
+        {
+            if (Ultimo == -1 && Primero == -1)
+                throw new Exception("No hay datos en la Pila");
+            else
+            {
+                if (Primero == Ultimo)
+                {
+                    Primero = -1;
+                    Ultimo = -1;
+                }
+                else
+                {
+                    Primero++;
+                }
+            }
+        }
+        public void Anadirdato(int numero)
+        {
+            if (Ultimo < (Dato.Length - 1))
+            {
+                Ultimo++;
+                Dato[Ultimo] = numero;
+            }
+        }
+
+    }
+}
---
+++ Programa_Integrado/Tads/Pila.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programa_Integrado.Tads
+{
+    public class Pila
+    {
+        private int[] dato = new int[10];
+        public int[] Dato
+        {
+            get { return dato; }
+            set { dato = value; }
+        }
+        private int ultimo = -1;
+
+        public int Ultimo
+        {
+            get { return ultimo; }
+            set { ultimo = value; }
+        }
+        private int primero = -1;
+
+        internal void pila()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Primero
+        {
+            get { return primero; }
+            set { primero = value; }
+        }
+
+        public void EliminarDato()
+        {
+            if (Ultimo == -1 && Primero == -1)
+                throw new Exception("No hay datos en la Pila");
+            else
+            {
+                if (Primero == Ultimo)
+                {
+                    Primero = -1;
+                    Ultimo = -1;
+                }
+                else
+                {
+                    Primero++;
+                }
+            }
+        }
+        public void Anadirdato(int numero)
+        {
+            if (Ultimo < (Dato.Length - 1))
+            {
+                Ultimo++;
+                Dato[Ultimo] = numero;
+            }
+        }
+
+    }
+}
